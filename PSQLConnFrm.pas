a0fb54f4ede1bae54e559cf1775ff4f87bb5220f
Mikhail Oleynik <m.oleynik@gmail.com>
2007-10-18 17:38:03 +0000

    root folders are being moved to trunk

---
+++ PSQLConnFrm.pas
@@ -0,0 +1,92 @@
+unit PSQLConnFrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, ExtCtrls,PSQLDbTables,PSQLTypes;
+
+type
+  TPSQLConnForm = class(TForm)
+    Bevel1: TBevel;
+    Label1: TLabel;
+    Label2: TLabel;
+    DBUserID: TEdit;
+    DBPasswd: TEdit;
+    Label3: TLabel;
+    DBName: TEdit;          
+    Label4: TLabel;
+    DBHost: TEdit;
+    Label5: TLabel;
+    DBPort: TEdit;
+    DBLogin: TCheckBox;
+    DBSSL: TCheckBox;
+    OkBtn: TButton;
+    CancelBtn: TButton;
+    Panel1: TPanel;
+  private
+    { Private declarations }
+    Database: TPSQLDatabase;
+    function Edit: Boolean;
+  public
+    { Public declarations }
+    procedure GetDatabaseProperty(Db: TPSQLDatabase);
+    procedure SetDatabaseProperty(Db: TPSQLDatabase);
+  end;
+
+function EditDatabase(ADatabase: TPSQLDatabase): Boolean;
+
+var
+  PSQLConnForm: TPSQLConnForm;
+
+implementation
+
+{$R *.DFM}
+
+function EditDatabase(ADatabase: TPSQLDatabase): Boolean;
+begin
+  with TPSQLConnForm.Create(Application) do
+  try
+    Database := ADatabase;
+    Result := Edit;
+  finally
+    Free;
+  end;
+end;
+
+function TPSQLConnForm.Edit: Boolean;
+begin
+  GetDatabaseProperty(Database);
+  Result := False;
+  if ShowModal = mrOk then
+  begin
+    SetDatabaseProperty(Database);
+    Result := True;
+  end;
+end;
+
+procedure TPSQLConnForm.GetDatabaseProperty(Db: TPSQLDatabase);
+begin
+  DBName.Text := DB.DatabaseName;
+  DBUserId.Text := db.UserName;
+  DBPasswd.Text := db.UserPassword;
+  DBHost.Text := Db.Host;
+  DBPort.Text := IntToStr(Db.Port);
+  DBLogin.Checked := db.LoginPrompt;
+  DBSSL.Checked := Db.UseSSL;
+end;
+
+procedure TPSQLConnForm.SetDatabaseProperty(Db: TPSQLDatabase);
+begin
+  DB.DatabaseName := DBName.Text;
+  db.UserName := DBUserId.Text;
+  db.UserPassword := DBPasswd.Text;
+  Db.Host := DBHost.Text;
+  Db.Port := StrToInt(DBPort.Text);
+  db.LoginPrompt := DBLogin.Checked;
+  Db.UseSSL := DBSSL.Checked;
+end;
+
+end.
+
+
