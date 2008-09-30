a8bab91e4eecc9093f0de58108988654585ca9a0
Pavlo Golub <pavlo.golub@gmail.com>
2008-09-30 08:23:36 +0000

    2.4.3 version

--- PSQLDbTables.pas
+++ PSQLDbTables.pas
@@ -12,7 +12,7 @@ Uses  Windows, SysUtils, Graphics, Classes, Controls, Db,
       PSQLCP, ExtCtrls;
 
 const
-    VERSION : string = '2.4.3-Dev';
+    VERSION : string = '2.4.3';
 
 { TDBDataSet flags }          
   dbfOpened     = 0;
