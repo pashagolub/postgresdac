e6d5c6893cd4dbbe983494a52b02bd13c23e9c4f
Pavlo Golub <pavlo.golub@gmail.com>
2009-07-06 06:21:29 +0000

    Version 2.5.2-beta2

--- PSQLDbTables.pas
+++ PSQLDbTables.pas
@@ -13,7 +13,7 @@ Uses  Windows, SysUtils, Graphics, Classes, Controls, Db,
       ExtCtrls;
 
 const
-    VERSION : string = '2.5.2-Beta1';
+    VERSION : string = '2.5.2-Beta2';
 
 { TDBDataSet flags }          
   dbfOpened     = 0;
