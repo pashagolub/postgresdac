33ac83bb9888d97ad7de3457ee99200cf67c6f89
Pavlo Golub <pavlo.golub@gmail.com>
2012-02-15 10:49:51 +0000

    Due to major changes version set to 2.8.0-beta

--- PSQLDbTables.pas
+++ PSQLDbTables.pas
@@ -14,7 +14,7 @@ uses  SysUtils, Classes, Db,
       PSQLAccess, PSQLTypes;
 
 const
-    VERSION : string = '2.7.0';
+    VERSION : string = '2.8.0-Beta';
     {$IFDEF MICROOLAP_BUSINESS_LICENSE}
     LICENSETYPE : string = 'Business License';
     {$ELSE}
