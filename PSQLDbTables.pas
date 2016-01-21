bf23af63cb2348a122f03c361f2368c4c2dab23b
Pavlo Golub <pavlo.golub@gmail.com>
2016-01-21 14:32:08 +0000

    3.1.0

--- PSQLDbTables.pas
+++ PSQLDbTables.pas
@@ -15,7 +15,7 @@ uses  SysUtils, Classes, Db,
       PSQLAccess, PSQLTypes;
 
 const
-    VERSION : string = '3.0.4';
+    VERSION : string = '3.1.0';
     {$IFDEF MICROOLAP_BUSINESS_LICENSE}
     LICENSETYPE : string = 'Business License';
     {$ELSE}
