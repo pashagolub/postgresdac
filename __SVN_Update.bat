6b0438ad98967dee82fb2a36c86abf2a06afa2b7
Pavlo Golub <pavlo.golub@gmail.com>
2022-03-17 18:23:52 +0100

    [-] remove old batch helpers

--- __SVN_Update.bat
+++
@@ -1,9 +0,0 @@
-@call __DelAllTemp.bat yes
-
-@echo ******************************************************************
-@echo ************************ TortoiseSVN *****************************
-@echo ******************************************************************
-
-@FOR /F "usebackq delims==" %%i IN (`cd`) DO SET PATHLOCAL=%%i
-
-TortoiseProc.exe /command:update /path:"%PATHLOCAL%" /notempfile /closeonend:2
